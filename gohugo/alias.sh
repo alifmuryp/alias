b3c7af318dfeeaf3f6865b9871e95e712cb29778
muryp <alifmuryp@gmail.com>
2021-03-15 16:57:09 +0700

    menyederhanakan addHugo

--- gohugo/alias.sh
+++ gohugo/alias.sh
@@ -1,18 +1,19 @@
-getHugo="source $getAlias/gohugo/"
+#getHugo="source $getAlias/gohugo/"
 ## ====== hugo pribadi
 alias st='hugo server --minify'
-alias buka='$getHugo/hugoTambah.sh && nano content/$file/$url.md' #img
-alias add='$getHugo/hugoTambah.sh' #img
-alias Ganti='$getHugo/hugoTambah.sh' #all
+#alias buka='$getHugo/hugoTambah.sh && nano content/$file/$url.md' #img
+#alias add='$getHugo/hugoTambah.sh' #img
+#alias Ganti='$getHugo/hugoTambah.sh' #all
 
-## hugo kusus
-alias addlms='$getHugo/pribadi/addLms.sh'
-alias addprojek='$getHugo/pribadi/addProjek.sh'
-alias addocs='$getHugo/pribadi/addDocs.sh'
-alias addblog='$getHugo/pribadi/addBlog.sh'
-alias addmuryp='mury $getHugo/addDefault.sh'
-alias addcode='code && $getHugo/addDefault.sh'
-alias addlms='lms && $getHugo/addDefault.sh'
+## hugo add
+alias addmuryp='mury hugo new '
+alias addcode='code && hugo new '
+alias addlms='lms && hugo new '
+## turunan
+alias addll='addlms && hugo new lms/'
+alias addprojek='addcode && hugo new projek/'
+alias addocs='addcode && hugo new docs/'
+alias addblog='mury hugo new blog/'
 
 
 # hugo sinkron
