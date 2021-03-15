ebb3d27e4f1ce0c6097e19cc439997e5a08d2695
muryp <alifmuryp@gmail.com>
2021-03-15 16:36:40 +0700

    membuat addhugo v.1

--- gohugo/alias.sh
+++ gohugo/alias.sh
@@ -1,8 +1,20 @@
+getHugo="source $getAlias/gohugo/"
 ## ====== hugo pribadi
 alias st='hugo server --minify'
-alias buka='source $getAlias/gohugo/hugoTambah.sh && nano content/$file/$url.md' #img
-alias add='source $getAlias/gohugo/hugoTambah.sh' #img
-alias Ganti='source $getAlias/gohugo/hugoTambah.sh' #all
+alias buka='$getHugo/hugoTambah.sh && nano content/$file/$url.md' #img
+alias add='$getHugo/hugoTambah.sh' #img
+alias Ganti='$getHugo/hugoTambah.sh' #all
+
+## hugo kusus
+alias addlms='$getHugo/pribadi/addLms.sh'
+alias addprojek='$getHugo/pribadi/addProjek.sh'
+alias addocs='$getHugo/pribadi/addDocs.sh'
+alias addblog='$getHugo/pribadi/addBlog.sh'
+alias addmuryp='mury $getHugo/addDefault.sh'
+alias addcode='code && $getHugo/addDefault.sh'
+alias addlms='lms && $getHugo/addDefault.sh'
+
+
 # hugo sinkron
 # alias sinkron='source ~/shared/.zzz/alias/sinkronFile.sh'
 # alias imgs='~/shared/.zzz/alias/./sinkronImg.sh'
